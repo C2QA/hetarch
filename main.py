43865cbc99ccaf6cbacf61729aae16ad93126c57
MADeMarco <michael@bluebonnetdata.com>
2023-01-21 18:33:58 -0500

    Refactor main.py to have parent transmon/cavity structures. Fixed passes to modules.

--- main.py
+++ main.py
@@ -5,25 +5,37 @@ from HarchSim.Device.Transmon import Transmon
 from HarchSim.Device.Cavity import Cavity
 from HarchSim.StandardCell.EPRGenerator import EPRGenerator
 from HarchSim.StandardCell.MemoryCell import MemoryCell
+from HarchSim.StandardCell.DistillationCell import DistillationCell
 from HarchSim.EntanglementDistillationController import EntanglementDistillationController
+
+from copy import copy
+
 import numpy as np
 
 """ Set an initial pure state of a bell pair in the state of |00> + |11> """
 
-
-qb1 = Transmon(1e-6, 1e-6)
-qb2 = Transmon(1e-6, 1e-6)
+"Set up transmon properties"
+qb = Transmon(1e-6, 1e-6)
 h = (1/np.sqrt(2))*np.array([[1, 1],
                              [1, -1]])
-qb1.add_gateset('h',h)
-qb2.add_gateset('h',h)
-epr_gen = EPRGenerator(qb1, qb2)
+qb.add_gateset('h',h)
+
+"Set up cavity properties"
+cav = Cavity(1e-6, 1e-6, 2, 2)
+
+"Set up EPR generator"
+epr_gen = EPRGenerator(copy(qb), copy(qb), copy(cav), copy(cav), 1e-6)
 epr_gen.entangle()
-cavity = Cavity(1e-6,1e-6,2,2)
-memory = MemoryCell(cavity,qb1)
+
+"Set up memory cell"
+memory = MemoryCell(copy(cav), copy(qb))
+
+"Set Up Distillation cell"
+distillation = DistillationCell(copy(qb), copy(qb))
 
 module = EntanglementDistillationController()
+module.modules = {"input": InputModule([epr_gen]),
+            "memory": MemoryModule([memory]),
+            "distillation": DistillationModule([distillation])}
 
-module.modules = {"input": InputModule(epr_gen),
-            "memory": MemoryModule(memory),
-            "distillation": DistillationModule()}
+module.run()
